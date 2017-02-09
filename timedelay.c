7796d8ba2bc7391faceb138fe6bce168ef5cf4a8
Marc Normandin <normandin.ubt@gmail.com>
2017-02-09 13:49:15 -0600

    timedelay finished

--- timedelay.c
+++ timedelay.c
@@ -12,33 +12,41 @@
 #include <gsl/gsl_matrix.h>
 #include <gsl/gsl_blas.h>
 
+// helper function
+static void set_wgs(double x, double y, double z, double* wgs) {
+    wgs[0] = x;
+    wgs[1] = y;
+    wgs[2] = z;
+}
+
 int timedelay(const char* Intefe_ID, double declination, double right_ascension, double *delay) 
 {
     // global WGS; // WGS-84 coordinates of beam splitter
+    double WGS[3];
 
     // One coordinate specified in the document for both H1 and H2)
     if (strcmp(Intefe_ID,'H1') == 0 || strcmp(Intefe_ID,'H2') == 0) {
-        WGS = [-2.161414928e+6, -3.834695183e+6, 4.600350224e+6]; // In meters
+        set_wgs(2.161414928e+6, -3.834695183e+6, 4.600350224e+6, &WGS); // In meters
     }
     else if (strcmp(Intefe_ID, 'L1') == 0) {
         // WGS-84 coordinates of 'L1' beam splitter
-        WGS = [-7.427604192e+4, -5.496283721e+6, 3.224257016e+6];
+        set_wgs(-7.427604192e+4, -5.496283721e+6, 3.224257016e+6, &WGS);
     }
     else if (strcmp(Intefe_ID, 'V1') == 0) {
         // WGS-84 coordinates of 'V1' beam splitter
-        WGS = [4.5463741e+6, 8.429897e+5, 4.378577e+6];
+        set_wgs(4.5463741e+6, 8.429897e+5, 4.378577e+6, &WGS);
     }
     else if (strcmp(Intefe_ID, 'G1') == 0) {
         // WGS-84 coordinates of 'G1' beam splitter
-        WGS = [3.8563112e+6,  6.665978e+5, 5.0196406e+6];
+        set_wgs(3.8563112e+6,  6.665978e+5, 5.0196406e+6, &WGS);
     }
     else if (strcmp(Intefe_ID, 'K1') == 0) {
         // WGS-84 coordinates of 'K1' beam splitter
-        WGS = [-3.776899062e+6,  3.483900163e+6, 3766657.585];
+        set_wgs(-3.776899062e+6,  3.483900163e+6, 3766657.585, &WGS);
     }
     else if (strcmp(Intefe_ID, 'T1') == 0) {
         // WGS-84 coordinates of 'T1' beam splitter
-        WGS = [-3.946409e6, 3.366259e6, 3.6991507e6];
+        set_wgs(-3.946409e6, 3.366259e6, 3.6991507e6, &WGS);    
     }
     else {
         GSL_ERROR("getifo: The Intefe_ID you entered is not currently listed.", GSL_EINVAL);
@@ -69,10 +77,10 @@ int timedelay(const char* Intefe_ID, double declination, double right_ascension,
     // should be one. (For testing purposes)
 
     //Define the GW's unit vector
-    const double gunit = sqrt( (gsl_pow_2(xgw) + gsl_pow_2(ygw) + gsl_pow_2(zgw) );
+    const double gunit = sqrt( gsl_pow_2(xgw) + gsl_pow_2(ygw) + gsl_pow_2(zgw) );
 
     // Define the speed of light
-    const double C=299792458; // m/s
+    const double C = 299792458; // m/s
 
     //Double check the negative sigh infront.
     // Shihan's expalnation: You don't have to put minus here. Plus or minus will be decided by the sky location
